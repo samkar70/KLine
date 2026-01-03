909cdfac18915fdedd63735851e433fbe0349f2d
samkar70 <samkar70@gmail.com>
2026-01-02 22:39:35 -0600

    Actualizacion automatica

--- src/data/database.ts
+++ src/data/database.ts
@@ -2,83 +2,43 @@ import { MediaItem } from '../types';
 
 export const mediaDatabase: MediaItem[] = [
   {
-    id: "v1767041095173",
-    title: "aaa",
-    type: "video",
-    category: "Momentos",
-    url: "https://www.youtube.com/live/fj_OXbFoLqg?si=DswkBVcyHPES1CQY",
-    thumbnail: "https://img.youtube.com/vi/fj_OXbFoLqg/maxresdefault.jpg",
-    artist: "Brandon Lake",
-    description: "aaaaaaaaaaaa"
-  },
-  {
-    id: "v1767041162077",
-    title: "bbbbbbb",
-    type: "video",
-    category: "Momentos",
-    url: "https://www.youtube.com/live/JNh-VILhFRE?si=AKuf63IGmpeAQI_s",
-    thumbnail: "https://img.youtube.com/vi/JNh-VILhFRE/maxresdefault.jpg",
-    artist: "Brandon Lake",
-    description: "bbbbbbbbbbbbb bbbbbbbbb"
-  },
-  {
-    id: "v1767041221800",
-    title: "ccccccccc",
-    type: "video",
-    category: "Momentos",
-    url: "https://www.youtube.com/live/2Fj4TFQ_fTE?si=QmapBbKPDtFq6oVe",
-    thumbnail: "https://img.youtube.com/vi/2Fj4TFQ_fTE/maxresdefault.jpg",
-    artist: "Brandon Lake",
-    description: "cccccccccccc"
-  },
-  {
-    id: "v1767041277864",
-    title: "mmmmmmmm",
-    type: "video",
-    category: "Entrevistas",
-    url: "https://youtube.com/shorts/cOe8PFbJCFo?si=IHPsz1EoDLGEw6Uj",
-    thumbnail: "https://img.youtube.com/vi/cOe8PFbJCFo/maxresdefault.jpg",
-    artist: "Brandon Lake",
-    description: "mmmmmmmmmm mmmmmmmmmmm"
-  },
-  {
-    id: "v1767041308609",
-    title: "mmmm",
-    type: "video",
-    category: "Entrevistas",
-    url: "https://youtube.com/shorts/Chmj74nfFMw?si=9fdZ8gj3Fy-IRgd_",
-    thumbnail: "https://img.youtube.com/vi/Chmj74nfFMw/maxresdefault.jpg",
-    artist: "Brandon Lake",
-    description: "mmmm  hhhh"
-  },
-  {
-    id: "v1767041374565",
-    title: "kkk",
+    id: "v1767414004049",
+    title: "Dios es quien te llama",
     type: "video",
     category: "General",
-    url: "https://youtu.be/hJz3NaeDRTA?si=nDxgEBuwegPGYlXh",
-    thumbnail: "https://img.youtube.com/vi/hJz3NaeDRTA/maxresdefault.jpg",
-    artist: "EnCanto Radio",
-    description: "kkk"
+    url: "https://youtu.be/O4g2if4tSyI?si=L8QgJ0XsqgTJ4lSq",
+    thumbnail: "https://img.youtube.com/vi/O4g2if4tSyI/maxresdefault.jpg",
+    artist: "samkar70",
+    description: "Olvida las etiquetas que te pone la gente. La única opinión que define tu destino es la de Dios y Él ya te ha declarado capaz valioso y lleno de honor."
   },
   {
-    id: "v1767041440086",
-    title: "k",
+    id: "v1767414183267",
+    title: "Angel Gabriel y Virgen Maria",
     type: "video",
-    category: "Entrevistas",
-    url: "https://www.youtube.com/live/4hvZ11J8aQI?si=6J2qd0zptef1xMXh",
-    thumbnail: "https://img.youtube.com/vi/4hvZ11J8aQI/maxresdefault.jpg",
-    artist: "Brandon Lake",
-    description: "kkkkk"
+    category: "Momentos",
+    url: "https://youtube.com/shorts/ffAvvebaxKg?si=_TXCttp9CVIPl-5w",
+    thumbnail: "https://img.youtube.com/vi/ffAvvebaxKg/maxresdefault.jpg",
+    artist: "samkar70",
+    description: "Un mensajero de luz una mujer de fe y un 'Sí' que abrazó al mundo entero. El momento exacto donde Dios se hizo cercano."
   },
   {
-    id: "v1767041471188",
-    title: "ooo",
+    id: "v1767414344742",
+    title: "Hay tiempos de Gloria para ti",
     type: "video",
-    category: "Musica",
-    url: "https://youtu.be/Kl1U2G2wNAo?si=-YlwwOtqv9gN_m5H",
-    thumbnail: "https://img.youtube.com/vi/Kl1U2G2wNAo/maxresdefault.jpg",
-    artist: "Brandon Lake",
-    description: "ooooooooooo"
+    category: "Momentos",
+    url: "https://youtube.com/shorts/URUk2qAZHIQ?si=_WTr55C8gQJjqphi",
+    thumbnail: "https://img.youtube.com/vi/URUk2qAZHIQ/maxresdefault.jpg",
+    artist: "samkar70",
+    description: "La prueba termina la promesa comienza. Créelo: hay tiempos de gloria para ti."
+  },
+  {
+    id: "v1767414450689",
+    title: "En Su amor somos dingos de ser",
+    type: "video",
+    category: "Momentos",
+    url: "https://youtube.com/shorts/CrnZrj9Oyzk?si=fDGRLnNOHYz_GdjJ",
+    thumbnail: "https://img.youtube.com/vi/CrnZrj9Oyzk/maxresdefault.jpg",
+    artist: "samkar70",
+    description: "Su gracia cubre nuestras faltas y nos devuelve la corona que perdimos. Créelo: en Su amor somos dignos de ser felices libres y plenos."
   }
 ];
